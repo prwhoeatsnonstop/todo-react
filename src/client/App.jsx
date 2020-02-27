006e6796981896338c9056139e85adf0f6e9cb2c
eunicelok <eunicelp511@gmail.com>
2020-02-28 07:08:10 +0800

    Removed class 'List', use class 'App', able to delete to do item

--- src/client/App.jsx
+++ src/client/App.jsx
@@ -4,7 +4,7 @@ import { hot } from 'react-hot-loader';
 class App extends React.Component {
     constructor(){
       super()
-
+// this.state is smth like a global value that you can access
       this.state = {
         currentInput: "",
         list: [],
@@ -12,52 +12,74 @@ class App extends React.Component {
       }
     }
 
+    handleClick(){
+      let toDo = this.state.currentInput;
+      if (toDo.length > 1 && toDo.length < 200) {
+        let toDoList = this.state.list;
+        toDoList.push({task: toDo});
+        this.setState({list:toDoList, errorText: ''});
+        this.setState({list: toDoList,currentInput: "", errorMessageToUser: ""});
+      }
+      else {
+        let errorMessageToUser = "The input must be more than 1 letter and maximum of 200 letters as limit!";
+        this.setState({errorText: errorMessageToUser, currentInput: " "});
+      }
+    }
+        //need smth that takes the input and push into a list and show it in a <ul></ul>
+
     changeHandler(event){
         this.setState({currentInput: event.target.value});
         console.log("change", event.target.value);
     }
 
-
-    handleClick(){
-      var todoList;
-      if (this.state.currentInput.length > 1 && this.state.currentInput.length < 200) {
-        todoList = this.state.list;
-        todoList.push(this.state.currentInput);
-        this.setState({list:todoList, errorText: ''});
-        let clearText = '';
-        this.setState({currentInput: clearText});
-      }
-      else {
-        let errorMessageToUser = "The input must be more than 1 letter and maximum of 200 letters as limit!"
-        this.setState({errorText: errorMessageToUser, currentInput: ''});
-      }
+    deleteItem(event) {
+        let todoItems = this.state.list;
+        todoItems.splice(parseInt(event.target.value), 1);
+        this.setState({list: todoItems});
     }
-        //need smth that takes the input and push into a list and show it in a <ul></ul>
 
     render() {
         console.log("rendering");
+        let displayErrorMsg;
+            if (this.state.errorText != "") {
+                displayErrorMsg = (<p className="warning">{this.state.errorText}</p>);
+            } else {
+                displayErrorMsg = "";
+            }
+    let counter = 0;
+    let listOfToDos = this.state.list.map(list=> {
+        counter ++;
+        return <div>
+                        <li>{list.task}</li>
+                        <button onClick={(event)=>{this.deleteItem(event)}} value={counter-1}>Remove</button>
+                        </div>
+    });
         return (
           <div className="item">
             <input onChange={(event)=>{this.changeHandler(event);}} value = {this.state.currentInput}/>
             <button onClick={()=>{this.handleClick()}}>Add To List!</button>
-            <List todo={this.state.list}/>
-            <p className="warning">{this.state.errorText}</p>
+            <div>
+                <ol>
+            {listOfToDos}
+            {displayErrorMsg}
+                </ol>
+            </div>
           </div>
         );
     }
 }
 
-class List extends React.Component {
-    render() {
-        let listOfItem = this.props.todo.map( item => {
-            return <li>{item}</li>
-        })
-        return (
-            <ol>
-            {listOfItem}
-            </ol>
-            )
-        }
-}
+// class List extends React.Component {
+//     render() {
+//         let listOfItem = this.props.todo.map( item => {
+//             return <li>{item}</li>
+//         })
+//         return (
+//             <ol>
+//             {listOfItem}
+//             </ol>
+//             )
+//         }
+// }
 
 export default hot(module)(App);
